090c835fedf06d9aee090c5457e0a12b338f6b53
chauhan176 <chauhananuj176@gmail.com>
2020-02-08 05:36:31 +0530

    models initialized

--- Adware/Advertiser/models.py
+++ Adware/Advertiser/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Advertiser(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    company_name = models.CharField(max_length=100)
+    phone = models.IntegerField(max_length=10)
--- Adware/Screens/models.py
+++ Adware/Screens/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from Advertiser.models import Advertiser
 
-# Create your models here.
+class Screens(models.Model):
+    owner_email = models.ForeignKey(Advertiser,on_delete=models.CASCADE)
+    location = models.TextField(max_length=100) # will be divided into more fields later

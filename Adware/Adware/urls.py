39787eaf4e5ee01c0fbe85852c3be96a849c0427
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-05-17 23:45:51 +0530

    static files fixes

--- Adware/Adware/urls.py
+++ Adware/Adware/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from . import views
 from Advertiser.views import media
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),

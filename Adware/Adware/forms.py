c7e82d0768c6f13710178dd06b599d50515e66ed
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-05-19 15:29:28 +0530

    Bootstrap modal form init

--- Adware/Adware/forms.py
+++ Adware/Adware/forms.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class UserForm(UserCreationForm):
+    type = forms.CharField(max_length=10)
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email' ]

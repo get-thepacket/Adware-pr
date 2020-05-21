34a48893864b22e3979cfdfb2a2da6653a50f8a5
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-05-21 10:36:16 +0530

    DB changes for screen module migration required

--- Adware/Screens/models.py
+++ Adware/Screens/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from uuid import uuid4
 from django.contrib.auth.models import User
 
 
 class Screens(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
-    location = models.TextField(max_length=100) # will be divided into more fields later
+    address = models.CharField(max_length=100)
+    landmarks = models.CharField(max_length=200)
+

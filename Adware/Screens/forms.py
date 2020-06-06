cd6fe65b2dbb0d2c5806a7bb0e49f5444e03b0b7
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-06-06 16:11:44 +0530

    DB changes for screens

--- Adware/Screens/forms.py
+++ Adware/Screens/forms.py
@@ -4,11 +4,12 @@ from .models import Screens
 
 class ScreenForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args,**kwargs)
+        super().__init__(*args, **kwargs)
         self.fields['description'].widget.attrs.update({'class': 'form-control'})
-        self.fields['address'].widget.attrs.update({'class':'form-control'})
-        self.fields['landmarks'].widget.attrs.update({'class' : 'form-control'})
-        
+        self.fields['address'].widget.attrs.update({'class': 'form-control'})
+        self.fields['landmarks'].widget.attrs.update({'class': 'form-control'})
+        self.fields['type'].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Screens
-        fields = ('description', 'address', 'landmarks')
+        fields = ('description', 'type', 'address', 'landmarks')
--- Adware/Screens/models.py
+++ Adware/Screens/models.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import User
 
 
 class Screens(models.Model):
+    category = (
+        ('Big', 'More than X area'),
+        ('Medium', 'Between X and Y'),
+        ('Small', 'Less than Y')
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(default='Big', choices=category, max_length=10)
     description = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     landmarks = models.CharField(max_length=200)
-

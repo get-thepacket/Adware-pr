706084d811416f1673caac1505ce2c4a4fd6f76f
get-thepacket <lakshyab.1999@gmail.com>
2020-06-10 14:51:49 +0530

    create adv form update

--- Adware/Advertiser/forms.py
+++ Adware/Advertiser/forms.py
@@ -3,6 +3,12 @@ from .models import AdMedia
 
 
 class AdMediaForm(forms.ModelForm):
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['file_name'].widget.attrs.update({'class':'form-control'})
+        self.fields['media'].widget.attrs.update({'class':'form-control'})
+
+
     class Meta:
         model = AdMedia
         fields = {'file_name', 'media'}

13810e13e84f505a229cf3da5d2887094d5040f5
chauhan176 <chauhananuj176@gmail.com>
2020-07-15 12:38:55 +0530

    Publish Add fix | toaster added

--- Adware/Advertiser/views.py
+++ Adware/Advertiser/views.py
@@ -100,6 +100,9 @@ def screen_select(request, ad_id):
     todo: user interactive page
     todo: geo-location based selection
     """
+    msg = request.GET.get('info', '')
+    msgtype = request.GET.get('msgtype', 'error')
+
     search = request.GET.get('search', '')
     Screen = [i for i in Screens.objects.all() if i.ad_available > 0]
     query_result = []
@@ -143,7 +146,9 @@ def screen_select(request, ad_id):
                    'bflag': big_size_flag,
                    'mflag': med_size_flag,
                    'sflag': sml_size_flag,
-                   'search': search})
+                   'search': search,
+                   'info':msg,
+                   'msgtype':msgtype})
 
 
 @login_required
@@ -163,18 +168,20 @@ def publish(request, ad_id, screen_id):
             break
     print(1, screen)
     print(2, ad)
-    if not screen or not ad or ad.username != request.user or screen.ad_available <=0:
+    if not screen or not ad or ad.username != request.user:
         # Screen or Ad not found.
         # Advertisement does not belong to the current user.
         # Or Screen is occupied fully.
-        return redirect('/adv/publish/' + str(ad_id) + '?status="Some Error Occurred"')
+        return redirect('/adv/publish/' + str(ad_id) + '?info=Some Error Occurred&msgtype=error')
+    if screen.ad_available <=0:
+        return redirect('/adv/publish/' + str(ad_id) + '?info=Screen not available&msgtype=warning')
     display = DisplaysAd()
     display.ad = ad
     display.screen = screen
     screen.ad_available = screen.ad_available - 1
     screen.save()
     display.save()
-    return redirect('/adv/publish/' + str(ad_id) + '?status="Advertisement uploaded"')
+    return redirect('/adv/publish/' + str(ad_id) + '?info=Advertisement Published&msgtype=success')
 
 
 @login_required

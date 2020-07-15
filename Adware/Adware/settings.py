3fa9c46c65baab98c9700a29d0e6e27c4b025c58
Lakshya <loki@pop-os.localdomain>
2020-07-15 22:45:09 +0530

    privacy and template edit

--- Adware/Adware/settings.py
+++ Adware/Adware/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Adware.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'./Advertiser/Templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

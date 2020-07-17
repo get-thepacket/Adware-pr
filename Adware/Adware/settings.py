ce7e4a0d519c749d2fde96b5a2079f773b0b621f
chauhan176 <chauhananuj176@gmail.com>
2020-07-17 16:34:47 +0530

    fix

--- Adware/Adware/settings.py
+++ Adware/Adware/settings.py
@@ -80,7 +80,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'adwareCorp@gmail.com'
-EMAIL_HOST_PASSWORD = 'wellitisadware123@'
+EMAIL_HOST_PASSWORD = 'checkdiscord'
 EMAIL_USE_SSL = False
 
 # Database

2f07dd783ae51149db9becd3bc86ee902a56a8b6
chauhan176 <chauhananuj176@gmail.com>
2020-07-18 14:37:08 +0530

    Your Advertisements Modal | Finalized

--- Adware/Advertiser/views.py
+++ Adware/Advertiser/views.py
@@ -37,19 +37,8 @@ def index(request):
             temp=[]
         else:
             temp.append(i)
-    if temp:
-        user_media_pair.append(temp)
-    """"
-    for i in range(1,len(user_media),2):
-        print(user_media[i])
-        user_media_pair.append([user_media[i-1],user_media[i]])
-
-        if i%2==1:
-            user_media_pair.append([user_media[len(user_media)-1],None])
-    if len(user_media)%2==1:
-        user_media_pair.append([user_media[len(user_media)-1],None])
-    """
     print(user_media_pair)
+    print(temp)
     subscription = []
     total_cost = 0
     for sub in DisplaysAd.objects.all():
@@ -61,6 +50,7 @@ def index(request):
                   {'user': str(request.user).split("@")[0],
                    'f1': form,
                    'AdMedia': user_media_pair,
+                   'temp' : temp,
                    'info': msg,
                    'msgtype': msgtype,
                    'total_cost':total_cost,

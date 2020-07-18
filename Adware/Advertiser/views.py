912dc7b3822dbb35022fc713445abb42e489bb43
chauhan176 <chauhananuj176@gmail.com>
2020-07-18 17:59:54 +0530

    Transaction cancelled | Temporary fix

--- Adware/Advertiser/views.py
+++ Adware/Advertiser/views.py
@@ -249,6 +249,7 @@ def handlerequest(request, screen_id):
             print('order successful')
         else:
             print('order was not successful because' + response_dict['RESPMSG'])
+            return redirect('/adv?info=Transaction Failed&msgtype=error')
     return render(request, 'Advertiser/paymentstatus.html', {'response': response_dict})
 
 
